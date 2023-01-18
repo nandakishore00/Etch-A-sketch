c2a1cf3cef12775f68ad7fd8ee01a4994a273a27
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-01-18 13:23:46 +0530

    add reset & pause events

--- index.js
+++ index.js
@@ -1,54 +1,79 @@
 let color='black'
-
+let click=false
+let clickToPause=document.querySelector("body")
+let reset= document.querySelector(".btn-primary")
 let grid=document.querySelector('.grid');
 let selectButton=document.querySelector(".btn-danger")
+reset.addEventListener("click",resetButton)
+
+clickToPause.addEventListener("click",function(e){
+    if(e.target.tagName!=="BUTTON"){
+        let pause=document.querySelector(".pause")
+        if(click==false){
+        pause.innerText="U can resume sketching"
+        click=true
+    }
+    else if(click ==true){
+        click= false
+        pause.innerText="Please click somewhere on the screen to resume sketching"
+    }
+    }
+})
 selectButton.addEventListener("click",function(){
     let getSize=selectSize();
     createGrid(getSize);
 
 })
-function createGrid(size){
-    document.querySelector('.grid').style.gridTemplateRows=`repeat(${size},1fr)`;
-    document.querySelector('.grid').style.gridTemplateColumns=`repeat(${size},1fr)`;
-    let numDiv=size*size;
-    for (var i=0;i<numDiv;i++){
-        let div=document.createElement("div");
-        div.addEventListener("mouseover",colorDiv)
-        grid.insertAdjacentElement("beforeend",div);
-        
-    }
-}
-function colorDiv(){
-
-if(color=='random'){
-    this.style.backgroundColor=`hsl(${Math.random()*360},100%,50%)`
-}
-else if(color=='black'){
-    this.style.backgroundColor='black';
-}
-}
-function setColor(colorChoice){
-   color=colorChoice
-}
 function selectSize()
 {
     let size=prompt("enter a number between 1 to 100")
     if(size==""){
         while(size==""){
             alert("please provide input")
-            size=prompt("enter a number between 1 to 64")
+            size=prompt("enter a number between 1 to 100")
         }
         
     }
-    else if(size<0 || size>64)
+    else if(size<0 || size>100)
     {
-        while(size<0 ||size>64)
+        while(size<0 ||size>100)
         {
-            alert("please provide input")
-            size=prompt("enter a number between 1 to 64")
+            alert("number should be within 0 to 100")
+            size=prompt("enter a number between 1 to 100")
         }
         
     }
     return size
 }
+function createGrid(size){
+    document.querySelector('.grid').style.gridTemplateRows=`repeat(${size},1fr)`;
+    document.querySelector('.grid').style.gridTemplateColumns=`repeat(${size},1fr)`;
+    let numDiv=size*size;
+    for (var i=0;i<numDiv;i++){
+        let div=document.createElement("span");
+        div.addEventListener("mouseover",colorDiv)
+        grid.insertAdjacentElement("beforeend",div); 
+    }
+}
+function colorDiv(){
+    if(click==true)
+    {
+        if(color=='random'){
+            this.style.backgroundColor=`hsl(${Math.random()*360},100%,50%)`
+        }
+        else if(color=='black'){
+            this.style.backgroundColor='black';
+        }
+    }
+}
+function setColor(colorChoice){
+   color=colorChoice
+}
+function resetButton(){
+    let divs=document.querySelectorAll("span")
+    divs.forEach((div)=>
+    div.style.backgroundColor="white")
+    
+}
+
 createGrid(16)
